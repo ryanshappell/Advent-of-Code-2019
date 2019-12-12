117126564e3c5a0cabd7365dd5f5300b3f165ffa
Ryan Shappell <rshapp@uw.edu>
2019-12-11 19:17:58 -0800

    day 2 pt 2

---
+++ day2_p2.py
@@ -0,0 +1,38 @@
+file = open("day2_input.txt", "r")
+original = file.read()
+orig_data = list(map(int, original.split(",")))
+file.close()
+
+def reset():
+    file = open("day2_input.txt", "w")
+    file.write(original)
+    file.close()
+    
+def check(noun, verb):
+    file = open("day2_input.txt", "r")
+    data = list(map(int, file.read().split(",")))
+
+    data[1] = noun
+    data[2] = verb
+
+    i = 0
+    while i < len(data):
+        if data[i] == 99:
+            break
+        if data[i] == 1:
+            data[data[i+3]] = data[data[i+1]] + data[data[i+2]]
+            i += 3
+        elif data[i] == 2:
+            data[data[i+3]] = data[data[i+1]] * data[data[i+2]]
+            i += 3
+        i += 1
+    file.close()
+    reset()
+    return data[0]
+
+for noun in range(100):
+    for verb in range(100):
+        if check(noun, verb) == 19690720:
+            print("FOUNDDD")
+            print(str(100 * noun + verb))
+            break
