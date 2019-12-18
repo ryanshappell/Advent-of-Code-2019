5dea8f01b55a53eb026f40c1115ae6146ea40083
Ryan Shappell <rshapp@uw.edu>
2019-12-18 18:18:49 +0200

    day 5 part 2

--- day5/intcode.py
+++ day5/intcode.py
@@ -25,20 +25,40 @@ class Intcode():
         return int(program[a]) if mode[-1] == "0" else a
     
     # Opcode 5
-    def jump_if_true(self):
-        pass
+    def jump_if_true(self, ptr, program, mode):
+        a = int(program[ptr[0]+1])
+        b = int(program[ptr[0]+2])
+        if (int(program[a]) if mode[-1] == "0" else a) != 0:
+            ptr[0] = int(program[b]) if mode[-2] == "0" else b
+        else:
+            ptr[0] += 3
 
     # Opcode 6
-    def jump_if_false(self):
-        pass
+    def jump_if_false(self, ptr, program, mode):
+        a = int(program[ptr[0]+1])
+        b = int(program[ptr[0]+2])
+        if (int(program[a]) if mode[-1] == "0" else a) == 0:
+            ptr[0] = int(program[b]) if mode[-2] == "0" else b
+        else:
+            ptr[0] += 3
 
     # Opcode 7
-    def less_than(self):
-        pass
+    def less_than(self, ptr, program, mode):
+        a = int(program[ptr+1])
+        b = int(program[ptr+2])
+        if (int(program[a]) if mode[-1] == "0" else a) < (int(program[b]) if mode[-2] == "0" else b):
+            program[int(program[ptr+3])] = 1
+        else:
+            program[int(program[ptr+3])] = 0
 
     # Opcode 8
-    def equals(self):
-        pass
+    def equals(self, ptr, program, mode):
+        a = int(program[ptr+1])
+        b = int(program[ptr+2])
+        if (int(program[a]) if mode[-1] == "0" else a) == (int(program[b]) if mode[-2] == "0" else b):
+            program[int(program[ptr+3])] = 1
+        else:
+            program[int(program[ptr+3])] = 0
 
     def opcode(self, p, program):
         ptr = p[0]
@@ -48,6 +68,7 @@ class Intcode():
             mode = "0" + mode
         if code == "99":
             print("HALT")
+            #print(program)
             exit()
         elif code == "1" or code == "01":
             self.add(ptr, program, mode)
@@ -56,21 +77,22 @@ class Intcode():
             self.multiply(ptr, program, mode)
             p[0] += 4
         elif code == "3" or code == "03":
-            self.store(1, ptr, program, mode)
+            self.store(5, ptr, program, mode)
             p[0] += 2
         elif code == "4" or code == "04":
             out = self.output(ptr, program, mode)
             print("output: " + str(out))
             p[0] += 2
         elif code == "5" or code == "05":
-            pass
+            self.jump_if_true(p, program, mode)
         elif code == "6" or code == "06":
-            pass
+            self.jump_if_false(p, program, mode)
         elif code == "7" or code == "07":
-            pass
+            self.less_than(ptr, program, mode)
+            p[0] += 4
         elif code == "8" or code == "08":
-            pass
-
+            self.equals(ptr, program, mode)
+            p[0] += 4
 
     def run(self):
         i = [0]
